c92687fc4038b105eb0f522747b24823c309f480
agostinopique <ago.pique@yahoo.it>
2022-04-26 15:24:21 +0200

    First commit; Create strutture base e logica discorsiva;

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* 
+**Descrizione:**
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
+Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+    1. Aggiungere un event listener per far partire il gioco al click del bottone;
+    2. Creare una timing function (setInterval) per la visualizzazioe dei numeri;
+    2 bis. Creare e appendee i cubi con i numeri generati randomicamente all'interno del contenitore;
+    3. Scaduto il tempo, rimuovere il testo all'interno dei cubi;
+    4. Triggerare i prompt per la richiesta dei numeri all'utente;
+    5. Verificare i numeri inseriti dall'utente con i numeri estratti in questa partita;
+    6. Mostrare le casistiche di vittoria o perdita;
+ */
+
